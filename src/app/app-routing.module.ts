ff5358a5ed72e2e794356f2c87582e4b02f8569b
kahenya-anita <anitakahenya1@gmail.com>
2020-08-31 19:59:01 +0300

    Import repo-search to routing and const the path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './Components/home/home.component';
 import { ProfileComponent } from './Components/profile/profile.component';
+import {RepoSearchComponent } from './Components/repo-search/repo-search.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent },
   { path: 'profile', component: ProfileComponent },
+  { path: 'repo-search', component: RepoSearchComponent},
 ];
 
 @NgModule({
