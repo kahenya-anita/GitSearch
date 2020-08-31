08a19954ea7cdd364393af936356814de8f79d2b
kahenya-anita <anitakahenya1@gmail.com>
2020-08-31 21:46:55 +0300

    Import elements from angular core

--- src/app/Directives/visited-repository.ts
+++ src/app/Directives/visited-repository.ts
@@ -1,2 +1,18 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+
+@Directive({
+    selector: '[appVisitedRepository]'
+  })
 export class VisitedRepository {
+    constructor(private elem:ElementRef) {}
+
+  @HostListener("click") onClicks(){
+    this.text("rgb(255, 20, 147")
+  }
+
+  private text(action:string){ 
+    this.elem.nativeElement.style.color=action;
+  }
+}
 }
