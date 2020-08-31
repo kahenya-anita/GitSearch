f55c7edc2e06fb3509dd2fc09f5c2570a4b02d72
kahenya-anita <anitakahenya1@gmail.com>
2020-08-31 22:30:12 +0300

    Add constructor to repositories

--- src/app/Model/repositories.ts
+++ src/app/Model/repositories.ts
@@ -1,2 +1,10 @@
 export class Repositories {
+    constructor(
+        public name:string,
+        public description:string,
+        public language:string,
+        public html_url: string,
+        public forks:number,
+        public updated_at:Date
+    ){}
 }
