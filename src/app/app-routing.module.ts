c89d1d10d26c22f2c77849216e090939c1381e91
kahenya-anita <anitakahenya1@gmail.com>
2020-08-31 21:53:10 +0300

    Add not-found component in routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './Components/home/home.component';
 import { ProfileComponent } from './Components/profile/profile.component';
-import {RepoSearchComponent } from './Components/repo-search/repo-search.component';
-
+import { RepoSearchComponent } from './Components/repo-search/repo-search.component';
+import { NotFoundComponent } from './Components/not-found/not-found/not-found.component'
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent },
   { path: 'profile', component: ProfileComponent },
   { path: 'repo-search', component: RepoSearchComponent},
+  { path: 'not-found', component: NotFoundComponent},
 ];
 
 @NgModule({
