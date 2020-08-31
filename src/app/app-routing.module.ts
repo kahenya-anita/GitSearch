b2100a8d07fb2e2248275f2c7c259620beea49f6
kahenya-anita <anitakahenya1@gmail.com>
2020-08-31 22:59:19 +0300

    Add searchresult to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './Components/home/home.component';
 import { ProfileComponent } from './Components/profile/profile.component';
 import { RepoSearchComponent } from './Components/repo-search/repo-search.component';
-import { NotFoundComponent } from './Components/not-found/not-found/not-found.component'
+import { NotFoundComponent } from './Components/not-found/not-found/not-found.component';
+import { SearchresultComponent } from './Components/searchresult/searchresult.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent },
   { path: 'profile', component: ProfileComponent },
-  { path: 'repo-search', component: RepoSearchComponent},
-  { path: 'not-found', component: NotFoundComponent},
+  { path: 'repo-search/:reponame', component: RepoSearchComponent },
+  { path: 'searchresult/:username', component: SearchresultComponent },
+  { path: 'not-found', component: NotFoundComponent },
 ];
 
 @NgModule({
