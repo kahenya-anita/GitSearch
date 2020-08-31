8e83dbd5bf8db5b027657732f0663c03d78cabdc
kahenya-anita <anitakahenya1@gmail.com>
2020-08-31 22:01:08 +0300

    Add class number-of-repos

---
+++ src/app/Model/number-of-repos.spec.ts
@@ -0,0 +1,7 @@
+import { NumberOfRepos } from './number-of-repos';
+
+describe('NumberOfRepos', () => {
+  it('should create an instance', () => {
+    expect(new NumberOfRepos()).toBeTruthy();
+  });
+});
