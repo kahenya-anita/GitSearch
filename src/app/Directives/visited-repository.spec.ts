e666e94016e6f236a593ae5a0a353b6b72f8199b
kahenya-anita <anitakahenya1@gmail.com>
2020-08-31 21:38:14 +0300

    Add directive class

---
+++ src/app/Directives/visited-repository.spec.ts
@@ -0,0 +1,7 @@
+import { VisitedRepository } from './visited-repository';
+
+describe('VisitedRepository', () => {
+  it('should create an instance', () => {
+    expect(new VisitedRepository()).toBeTruthy();
+  });
+});
