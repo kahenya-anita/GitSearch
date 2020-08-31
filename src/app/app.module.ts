b7cf6cc53d47a9bc67d894ad8e3dcde0662f883c
kahenya-anita <anitakahenya1@gmail.com>
2020-08-31 17:16:59 +0300

    Add repo-search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './Components/home/home.component';
 import { ProfileComponent } from './Components/profile/profile.component';
 import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
+import { RepoSearchComponent } from './Components/repo-search/repo-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ProfileComponent,
-    NavBarComponent
+    NavBarComponent,
+    RepoSearchComponent
   ],
   imports: [
     BrowserModule,
