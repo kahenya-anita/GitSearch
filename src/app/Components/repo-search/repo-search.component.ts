c7d5ae3947020973808a6d9bba24aca62e294668
kahenya-anita <anitakahenya1@gmail.com>
2020-09-01 11:59:10 +0300

    Add imports to searchresult

--- src/app/Components/repo-search/repo-search.component.ts
+++ src/app/Components/repo-search/repo-search.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchGitService } from '../../services/search-git';
+import { Repositories } from '../../Model/repositories';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RepositoriesByName } from '../../Model/repositories-by-name';
+import { Users } from '../../Model/users';
 
 @Component({
   selector: 'app-repo-search',
@@ -6,10 +11,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./repo-search.component.css']
 })
 export class RepoSearchComponent implements OnInit {
+  
+  repos:Repositories[];
+  user:Users;
+  username:string;
+  
+  constructor( private route: ActivatedRoute, private searchGitService: SearchGitService ) {}
 
-  constructor() { }
+  searchResult(){
+    this.username = this.route.snapshot.paramMap.get('username')
+    this.searchGitService.userInfoRequest(this.username)
+    this.user = this.searchGitService.user
+    this.searchGitService.userRepoRequest(this.username)
+    this.repos =this.searchGitService.repos
+  }
 
-  ngOnInit(): void {
+  ngOnInit(){
+    this.searchResult()
   }
 
 }
